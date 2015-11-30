42ba0ab4912e0bceadbeef3c9f2f15668578a00c
fbid <federico.bidoggia@gmail.com>
2015-11-30 17:31:04 +0100

    fine sec4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,57 +1,60 @@
 angular.module('stocksApp.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl',['$scope','$ionicModal','$timeout',
+  function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Open the login modal
+    $scope.login = function() {
+      $scope.modal.show();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
+}])
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+.controller('MyStocksCtrl',['$scope',
+  function($scope) {
+    $scope.myStocksArray = [
+      {ticker: 'AAPL'},
+      {ticker: 'GPRO'},
+      {ticker: 'FB'},
+      {ticker: 'NFLX'},
+      {ticker: 'TSLA'},
+      {ticker: 'BBK-A'},
+      {ticker: 'MSFT'},
+      {ticker: 'GE'},
+      {ticker: 'BAC'},
+      {ticker: 'C'},
+      {ticker: 'T'}
+    ];
+}])
 
-.controller('MyStocksCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+.controller('StockCtrl',['$scope','$stateParams',
+  function($scope, $stateParams) {
 
-.controller('StockCtrl', function($scope, $stateParams) {
+    $scope.ticker = $stateParams.stockTicker;
 
-});
+}]);
