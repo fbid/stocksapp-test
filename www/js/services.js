eaae742ca6f64ba14900e11cd72a7a416897cbf3
fbid <federico.bidoggia@gmail.com>
2015-12-01 02:15:41 +0100

    Aggiunto service EncodeURI per la codifica di %20 e %22 nella querystring YQL

--- www/js/services.js
+++ www/js/services.js
@@ -1,12 +1,28 @@
 angular.module('stocksApp.services',[])
 
-.factory('StockDataService',function($q, $http){
+.factory('EncodeURIService',function(){
+
+    return {
+      encode: function(string){
+        console.log(string);
+        var encoded_string = encodeURIComponent(string)
+          .replace(/\"/g,"%22")
+          .replace(/\ /g,"%20")
+          .replace(/[!'()]/g, escape)
+          ;
+
+        return encoded_string;
+      }
+    };
+})
+
+.factory('StockDataService',function($q, $http, EncodeURIService){
 
   //funzione che fa il fetch dei dettagli dello stock
   var getDetailsData = function(ticker){
     var d = $q.defer();
-
-    var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22'+ticker+'%22)&format=json&env=http://datatables.org/alltables.env';
+    var query = 'select * from yahoo.finance.quotes where symbol IN ("'+ticker+'")';
+    var url = 'http://query.yahooapis.com/v1/public/yql?q='+ EncodeURIService.encode(query) +'&format=json&env=http://datatables.org/alltables.env';
 
     $http.get(url)
       .success(function (json){
